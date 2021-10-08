0778d6741f01cb5f5428ba5ab56b50e9addebb37
Luis <luis.pulido.d@bairesdev.com>
2021-10-08 11:15:19 -0700

    Added logger module which uses logic from standard logger.rb ruby class

---
+++ lib/logging.rb
@@ -0,0 +1,12 @@
+require 'logger'
+module Logging
+	def validate_level(level)
+		unless level.between?(Logger::DEBUG, Logger::FATAL)
+			raise ArgumentError, "invalid log level #{level}"
+		end
+	end
+	def log(message, level=Logger::INFO)
+		validate_level level
+		Logger.new(STDOUT).add(level){ message }
+	end
+end
