3fd872f0e191a22dde3caf13b869f3e7ad4738d4
Luis Pulido <luis.pulido.d@bairesdev.com>
2021-10-13 12:14:34 -0700

    fixed line endings and indentation

--- run.rb
+++ run.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require 'json'
-
-require './calc'
-require './team'
-require './tournament'
+require 'tournament'
 
 json = JSON.parse(File.read('input.json'))
 
-program = TournamentGem.new(json).print_table
+program = Tournament.run(json)
